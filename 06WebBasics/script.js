8cfc8c7d6f6ced0c566bfc0bed2298da374f295f
Abdullah Deshmukh <abdullahzakir99@gmail.com>
2019-09-07 20:32:32 +0530

    Log basic DOM things

--- 06WebBasics/script.js
+++ 06WebBasics/script.js
@@ -1,2 +1,9 @@
 // Show a popup that the file is connected
-alert("File is attached")
+// alert("File is attached")
+
+// Log the document details
+console.log(document.head);
+
+console.log(document.title);
+
+console.log(document.body);
