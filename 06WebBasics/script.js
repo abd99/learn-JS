237bbf83a2b06cd86b82cf9d6b120928d7365d93
Abdullah Deshmukh <abdullahzakir99@gmail.com>
2019-09-13 22:44:43 +0530

    Add change text at runtime using JS

--- 06WebBasics/script.js
+++ 06WebBasics/script.js
@@ -2,8 +2,19 @@
 // alert("File is attached")
 
 // Log the document details
-console.log(document.head);
+// console.log(document.head);
 
-console.log(document.title);
+// console.log(document.title);
 
-console.log(document.body);
+// console.log(document.body);
+
+// Create an object of the paragraph content
+const myPElements = document.querySelector('p')
+// change the textContent of the object - It reflects directly at the web page
+myPElements.textContent = 'This is the new paragraph text changed at runtime using JS'
+// Note - Comment out/remove below lines of code to check the working of above two lines
+
+// Define an object getting an array of all p tags
+const allPElements = document.querySelectorAll('p')
+
+allPElements.forEach((p) => p.textContent = "This is changed using Loop in JS")
