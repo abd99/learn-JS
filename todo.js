60989f5a04fbdafd43f67aadf24628e296b0eb74
Abdullah Deshmukh <abdullahzakir99@gmail.com>
2019-11-06 20:14:38 +0530

    Implement localStorage for adding, loading todos

--- todo.js
+++ todo.js
@@ -9,6 +9,14 @@ removeButton.addEventListener('click', removeItem)
 // Get the object of the entire list
 let ul = document.getElementById('list')
 
+// Todo Object to store data
+var todosObject = {
+    names: []
+}
+
+// Load existing todos from localStorage
+loadFromLocalStorage()
+
 // function to add new tasks
 function addItem() {
     // Get the input entered by the user
@@ -34,6 +42,18 @@ function addItem() {
         return
 
     } else {
+
+        // Inserting the new todo into localStorage
+        // Read JSONString from localStorage
+        let todosJSONString = localStorage.getItem('todos')
+        // Check if data exists or not
+        if(todosJSONString != null)
+            todosObject = JSON.parse(todosJSONString)
+        //Append the new todo into the object 
+        todosObject.names.push(textValue)
+        console.log(todosObject);
+        // Store new todoObject into localStorage in JSON String form
+        localStorage.setItem('todos', JSON.stringify(todosObject))
 
         // Create a new list item
         let li = document.createElement('li')
@@ -76,4 +96,41 @@ function removeItem() {
             ul.removeChild(li[index])               
         }
     }
+}
+
+function loadFromLocalStorage() {
+    // Get data from localStorage
+    todosObject = JSON.parse(localStorage.getItem('todos'))
+    // Get the todo from the localStorage
+    todosObject.names.forEach((todoName) => {
+
+        let textValue = todoName
+        // Create a textNode with the value entered by user
+        let textNode = document.createTextNode(textValue)
+        console.log(textNode);
+        
+        // Create a new list item
+        let li = document.createElement('li')
+        // Create a new checkbox
+        let checkbox = document.createElement('input')
+        checkbox.type = 'checkbox'
+        checkbox.setAttribute('id', 'check')
+        // Create a new label
+        let label = document.createElement('label')
+
+        // Add the things to view
+        // Append the checkbox into the list item
+        li.appendChild(checkbox)
+        // Append the textNode into the label item
+        label.appendChild(textNode)
+        // Append the label into the list item
+        li.appendChild(label)
+        // Insert the new list item before the existing first (0th) list item in the ul
+        ul.insertBefore(li, ul.childNodes[0])
+        setTimeout(() => {
+            // Show after a delay of 5 ms
+            li.className = 'visual'
+        }, 3);
+    
+    });
 }
